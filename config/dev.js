8c808fa4ffbb0c37bf30ea6213ff561294b2e390
vipulgiakwad07 <37871301+vipulgiakwad07@users.noreply.github.com>
2021-12-10 18:01:49 +0530

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+const { JWT_SECRET } = require("./prod");
+
+module.exports ={
+    MONGOURI:"mongodb+srv://vipul:vipuldb@cluster0.1arcf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET:"effegwegwrgrgw4",
+    SENDGRID_API:"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",     //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",
+    //SENDGRID_API:"gzR8qlGxQRGTHhjJAfxGhQ",
+    EMAIL:"http:localhost:3000"
+
+}
